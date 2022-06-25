ba29a6ff9311c579f061d7f85cfdf935ec8b82f6
ndesai <nickydesai.mail@gmail.com>
2022-06-25 17:45:06 -0500

    added setup files

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-console.log('app.js is ooonnnn');
+const express = require('express');
+const app = express();
 
 let port = process.env.PORT;
 if (port == null || port == '') {
   port = 8000;
 }
-app.listen(port);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
