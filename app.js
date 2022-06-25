6c5cbf0299ae03ede5ade9250d8391dea5ea812b
ndesai <nickydesai.mail@gmail.com>
2022-06-25 17:48:46 -0500

    added setup files

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 let port = process.env.PORT;
 if (port == null || port == '') {
@@ -7,7 +8,11 @@ if (port == null || port == '') {
 }
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('fuck you!');
+});
+// sendFile will go here
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.listen(port, () => {
