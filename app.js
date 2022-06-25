7a6e60aa0768eacabbf73cf115e37344061544f3
ndesai <nickydesai.mail@gmail.com>
2022-06-25 17:49:30 -0500

    added setup files

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
 });
 // sendFile will go here
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
+  res.sendFile(path.join(__dirname, '/frontend/index.html'));
 });
 
 app.listen(port, () => {
